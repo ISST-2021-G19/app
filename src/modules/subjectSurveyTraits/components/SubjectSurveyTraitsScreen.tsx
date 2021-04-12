d1c1daed65687d28ef25d3a4a85752f984b894b7
Irene Enamorado <irenlay@gmail.com>
2021-04-12 23:38:09 +0200

    Add styled-components, useEffect and nav buttons

--- src/modules/subjectSurveyTraits/components/SubjectSurveyTraitsScreen.tsx
+++ src/modules/subjectSurveyTraits/components/SubjectSurveyTraitsScreen.tsx
@@ -1,78 +1,179 @@
-import React, { useState } from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
 import * as S from "./SubjectSurveyTraits.style";
 
 function SubjectSurveyTraitsScreen() {
-  const [atribute, setAtribute] = useState("Hola");
+  const [atribute, setAtribute] = useState("");
+
+  const AZUL_BASE = "#2368AF";
+  const GRADIENTE1 = "#679ECD";
+
+  const [color1, setColor1] = useState(AZUL_BASE);
+  const [color2, setColor2] = useState(AZUL_BASE);
+  const [color3, setColor3] = useState(AZUL_BASE);
+  const [color4, setColor4] = useState(AZUL_BASE);
+  const [color5, setColor5] = useState(AZUL_BASE);
+  const [color6, setColor6] = useState(AZUL_BASE);
+  const [color7, setColor7] = useState(AZUL_BASE);
+  const [color8, setColor8] = useState(AZUL_BASE);
+  const [color9, setColor9] = useState(AZUL_BASE);
+  const [color10, setColor10] = useState(AZUL_BASE);
+
+  useEffect(() => {
+    if (atribute.toLowerCase().includes("cercano")) {
+      setColor1(GRADIENTE1);
+    } else {
+      setColor1(AZUL_BASE);
+    }
+    if (atribute.toLowerCase().includes("impuntual")) {
+      setColor2(GRADIENTE1);
+    } else {
+      setColor2(AZUL_BASE);
+    }
+    if (atribute.toLowerCase().includes("accesible para tutorías")) {
+      setColor3(GRADIENTE1);
+    } else {
+      setColor3(AZUL_BASE);
+    }
+    if (atribute.toLowerCase().includes("clases amenas")) {
+      setColor4(GRADIENTE1);
+    } else {
+      setColor4(AZUL_BASE);
+    }
+    if (atribute.toLowerCase().includes("desorganizado")) {
+      setColor5(GRADIENTE1);
+    } else {
+      setColor5(AZUL_BASE);
+    }
+    if (atribute.toLowerCase().includes("se prepara las clases")) {
+      setColor6(GRADIENTE1);
+    } else {
+      setColor6(AZUL_BASE);
+    }
+    if (atribute.toLowerCase().includes("se pasa de la hora")) {
+      setColor7(GRADIENTE1);
+    } else {
+      setColor7(AZUL_BASE);
+    }
+    if (atribute.toLowerCase().includes("clases poco dinámicas")) {
+      setColor8(GRADIENTE1);
+    } else {
+      setColor8(AZUL_BASE);
+    }
+    if (atribute.toLowerCase().includes("buenos apuntes")) {
+      setColor9(GRADIENTE1);
+    } else {
+      setColor9(AZUL_BASE);
+    }
+    if (atribute.toLowerCase().includes("poco material de estudio")) {
+      setColor10(GRADIENTE1);
+    } else {
+      setColor10(AZUL_BASE);
+    }
+  });
 
   return (
-    <View>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "center",
-        }}
-      >
-        <S.ButtonTraits onPress={() => setAtribute(atribute + "Atributo 1, ")}>
-          Atributo 1
+    <S.Container>
+      <S.ContainerRow>
+        <S.ButtonTraits
+          style={{ backgroundColor: color1 }}
+          onPress={() => {
+            setAtribute(atribute + "Cercano, ");
+            setColor1(AZUL_BASE);
+          }}
+        >
+          Cercano
         </S.ButtonTraits>
-        <S.ButtonTraits onPress={() => setAtribute(atribute + "Atributo 2, ")}>
-          Atributo 2
+        <S.ButtonTraits
+          style={{ backgroundColor: color2 }}
+          onPress={() => {
+            setAtribute(atribute + "Impuntual, ");
+            setColor2(AZUL_BASE);
+          }}
+        >
+          Impuntual
         </S.ButtonTraits>
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "center",
-        }}
-      >
-        <S.ButtonTraits onPress={() => setAtribute(atribute + "Atributo 3, ")}>
-          Atributo 3
+      </S.ContainerRow>
+      <S.ContainerRow>
+        <S.ButtonTraits
+          style={{ backgroundColor: color3 }}
+          onPress={() => {
+            setAtribute(atribute + "Accesible para tutorías, ");
+            setColor2(AZUL_BASE);
+          }}
+        >
+          Accesible para tutorías
         </S.ButtonTraits>
-        <S.ButtonTraits onPress={() => setAtribute(atribute + "Atributo 4, ")}>
-          Atributo 4
+        <S.ButtonTraits
+          style={{ backgroundColor: color4 }}
+          onPress={() => {
+            setAtribute(atribute + "Clases amenas, ");
+            setColor2(AZUL_BASE);
+          }}
+        >
+          Clases amenas
         </S.ButtonTraits>
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "center",
-        }}
-      >
-        <S.ButtonTraits onPress={() => setAtribute(atribute + "Atributo 5, ")}>
-          Atributo 5
+      </S.ContainerRow>
+      <S.ContainerRow>
+        <S.ButtonTraits
+          style={{ backgroundColor: color5 }}
+          onPress={() => {
+            setAtribute(atribute + "Desorganizado, ");
+            setColor2(AZUL_BASE);
+          }}
+        >
+          Desorganizado
         </S.ButtonTraits>
-        <S.ButtonTraits onPress={() => setAtribute(atribute + "Atributo 6, ")}>
-          Atributo 6
+        <S.ButtonTraits
+          style={{ backgroundColor: color6 }}
+          onPress={() => {
+            setAtribute(atribute + "Se prepara las clases, ");
+            setColor2(AZUL_BASE);
+          }}
+        >
+          Se prepara las clases
         </S.ButtonTraits>
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "center",
-        }}
-      >
-        <S.ButtonTraits onPress={() => setAtribute(atribute + "Atributo 7, ")}>
-          Atributo 7
+      </S.ContainerRow>
+      <S.ContainerRow>
+        <S.ButtonTraits
+          style={{ backgroundColor: color7 }}
+          onPress={() => {
+            setAtribute(atribute + "Se pasa de la hora, ");
+            setColor2(AZUL_BASE);
+          }}
+        >
+          Se pasa de la hora
         </S.ButtonTraits>
-        <S.ButtonTraits onPress={() => setAtribute(atribute + "Atributo 8, ")}>
-          Atributo 8
+        <S.ButtonTraits
+          style={{ backgroundColor: color8 }}
+          onPress={() => {
+            setAtribute(atribute + "Clases poco dinámicas, ");
+            setColor2(AZUL_BASE);
+          }}
+        >
+          Clases aburridas
         </S.ButtonTraits>
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "center",
-        }}
-      >
-        <S.ButtonTraits onPress={() => setAtribute(atribute + "Atributo 9, ")}>
-          Atributo 9
+      </S.ContainerRow>
+      <S.ContainerRow>
+        <S.ButtonTraits
+          style={{ backgroundColor: color9 }}
+          onPress={() => {
+            setAtribute(atribute + "Buenos apuntes, ");
+            setColor2(AZUL_BASE);
+          }}
+        >
+          Buenos apuntes
         </S.ButtonTraits>
-        <S.ButtonTraits onPress={() => setAtribute(atribute + "Atributo 10, ")}>
-          Atributo 10
+        <S.ButtonTraits
+          style={{ backgroundColor: color10 }}
+          onPress={() => {
+            setAtribute(atribute + "Poco material de estudio, ");
+            setColor2(AZUL_BASE);
+          }}
+        >
+          Poco material de estudio
         </S.ButtonTraits>
-      </View>
-      <View style={{ flexDirection: "row", justifyContent: "center" }}>
+      </S.ContainerRow>
+      <S.ContainerRow>
         <S.CommentsBox
           multiline={true}
           numberOfLines={4}
@@ -80,8 +181,12 @@ function SubjectSurveyTraitsScreen() {
           onChangeText={setAtribute}
           value={atribute}
         ></S.CommentsBox>
-      </View>
-    </View>
+      </S.ContainerRow>
+      <S.ContainerRow>
+        <S.GoBackButton>Atrás</S.GoBackButton>
+        <S.SubmitButton>Enviar</S.SubmitButton>
+      </S.ContainerRow>
+    </S.Container>
   );
 }
 
